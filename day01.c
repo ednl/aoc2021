6105654e113be5883b156bf0ae8179fc50773ee2
E. Dronkert <ednl@users.noreply.github.com>
2021-12-01 08:31:27 +0100

    error checking

--- day01.c
+++ day01.c
@@ -13,10 +13,17 @@ static int increased(int window)
 
 int main(void)
 {
-    int i = 0;
     FILE *f = fopen("input01.txt", "r");
-    while (fscanf(f, "%d ", &depth[i++]) == 1);
+    if (!f)
+        return 1;
+
+    int i = 0;
+    while (i < N && fscanf(f, "%d ", &depth[i]) == 1)
+        ++i;
     fclose(f);
+    if (i != N)
+        return 2;
+
     printf("Part 1: %d\n", increased(1));
     printf("Part 2: %d\n", increased(3));
     return 0;
