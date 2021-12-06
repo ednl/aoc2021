56d31241263c93925ee7aaf6527347c359770c4f
E. Dronkert <ednl@users.noreply.github.com>
2021-12-06 13:48:55 +0100

    unix timer

--- day06.c
+++ day06.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
 
 #define CYCLE   7  // spawn every 7 days
 #define DELAY   2  // add 2 days to the first spawn cycle
@@ -9,6 +11,23 @@
 
 // Histogram of fish population count per age mod 9
 static uint64_t age[LIFE] = {0};
+
+static double timer(void)
+{
+    static bool start = true;
+    static struct timespec t0;
+
+    if (start) {
+        start = false;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+        return 0;
+    } else {
+        struct timespec t1;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+        start = true;
+        return 1.0 * t1.tv_sec + 1e-9 * t1.tv_nsec - (1.0 * t0.tv_sec + 1e-9 * t0.tv_nsec);
+    }
+}
 
 // Live for days, return population
 static uint64_t live(const int days)
@@ -26,6 +45,8 @@ static uint64_t live(const int days)
 
 int main(void)
 {
+    timer();
+
     // Build population histogram by age bin
     FILE *f = fopen("input06.txt", "r");
     int c = ',';
@@ -37,5 +58,6 @@ int main(void)
 
     printf("Part 1: %llu\n", live(DAYS1));  // 374927
     printf("Part 2: %llu\n", live(DAYS2));  // 1687617803407
+    printf("Time: %.6f s\n", timer());
     return 0;
 }
