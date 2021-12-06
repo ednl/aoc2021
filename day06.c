d85308cf78abbb07c80fa0549ef64fa2b4d330ce
E. Dronkert <ednl@users.noreply.github.com>
2021-12-06 10:47:43 +0100

    comments

--- day06.c
+++ day06.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define CYCLE 7
-#define DELAY 2
-#define LIFE  (CYCLE + DELAY)
-#define DAYS1 80
-#define DAYS2 256
+#define CYCLE   7  // give birth every 7 days
+#define DELAY   2  // add 2 days to the first birth cycle
+#define LIFE (CYCLE + DELAY)  // the whole circle
+#define DAYS1  80  // part 1
+#define DAYS2 256  // part 2
 
+// Population histogram per age (mod 9)
 static uint64_t pop[LIFE] = {0};
 
+// Live for X days, return total population
+// birth = histogram index of lantern fish that are
+//         about to give birth (countdown = 0)
+// Birth index advances every day <=> countdown to next birth
 static uint64_t live(int birth, int days)
 {
     for (int i = birth; i < birth + days; ++i)
+        // Everyone whose birth countdown is 0 starts a new cycle
         pop[(i + CYCLE) % LIFE] += pop[i % LIFE];
+        // Leaving the population of this index untouched means:
+        // removing them from this bin and adding their offspring
+        // to (what is the next) bin-1 = net zero effect
+
     uint64_t census = 0;
     for (int i = 0; i < LIFE; ++i)
         census += pop[i];
@@ -21,6 +31,8 @@ static uint64_t live(int birth, int days)
 
 int main(void)
 {
+    // Build population histogram by age bin
+    // by reading single-digit ages from CSV
     FILE *f = fopen("input06.txt", "r");
     int c = ',';
     while (c == ',') {
