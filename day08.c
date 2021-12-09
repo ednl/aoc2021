ef62138c072534897cc97faca4dcb8c6dc4737c4
E. Dronkert <ednl@users.noreply.github.com>
2021-12-09 04:43:35 +0100

    clearer & more concise

--- day08.c
+++ day08.c
@@ -12,58 +12,60 @@ static size_t len[N][M] = {0};  // avoid calling strlen every time
 // Does A contain B? (unsorted so has to check the whole range every time)
 static bool contains(const char *a, const char *b)
 {
-    while (*b) {
-        const char *c = a;
-        while (*c && *c != *b)
+    while (b && *b) {           // while b has chars
+        const char *c = a;      // a is needed to restart every loop so don't use it to walk through a[]
+        while (*c && *c != *b)  // find matching char or end up at \0 terminator
             c++;
-        if (!*c)
+        if (!*c)                // if char from b not found in a
             return false;
-        b++;
+        b++;                    // next char from b
     }
-    return true;
+    return true;                // all chars from b were found in a
 }
 
 int main(void)
 {
     int ans = 0;
     FILE *f = fopen("input08.txt", "r");
-    for (int i = 0; i < N &&
-        fscanf(f, "%7s %7s %7s %7s %7s %7s %7s %7s %7s %7s | %7s %7s %7s %7s ",
-            seg[i][0], seg[i][1], seg[i][2], seg[i][3], seg[i][4], seg[i][5], seg[i][6], seg[i][7], seg[i][8], seg[i][9],
-            seg[i][10], seg[i][11], seg[i][12], seg[i][13]) == 14; ++i) {
-        for (int j = 0; j < M; ++j)
-            len[i][j] = strlen(seg[i][j]);
-        for (int j = 10; j < M; ++j) {
-            if ((len[i][j] >= 2 && len[i][j] <= 4) || len[i][j] == 7)
-                ++ans;
-        }
+    // Read N lines of M strings
+    for (int i = 0, j = 0; i < N && fscanf(f, "%7s ", seg[i][j]); ++j == M && ++i && (j = 0)) {
+        size_t k = len[i][j] = strlen(seg[i][j]);
+        if (j == 9)
+            fgetc(f);  // discard '|'
+        if (j >= 10 && ((k >= 2 && k <= 4) || k == 7))
+            ++ans;
     }
     fclose(f);
     printf("Part 1: %d\n", ans);  // 534
 
-    // len 2: "1" = ..c..f.
-    // len 3: "7" = a.c..f.
-    // len 4: "4" = .bcd.f.
-    // len 5: "2" = a.cde.g
-    //        "5" = ab.d.fg
-    //        "3" = a.cd.fg
-    // len 6: "0" = abc.efg
-    //        "6" = ab.defg
-    //        "9" = abcd.fg
-    // len 7: "8" = abcdefg
-
     ans = 0;
     for (int i = 0; i < N; ++i) {
-        char seg1[L + 1], seg4[L + 1], seg9[L + 1];  // deciders
+        // Segment patterns 1, 4, 9 used as deciders
+        char pat1[L + 1], pat4[L + 1], pat9[L + 1];
+
+        // Find three deciders in first 10 digits
+        // (two loops because to find 9, 4 is needed first)
         for (int j = 0; j < 10; ++j)
             switch (len[i][j]) {
-                case 2: strcpy(seg1, seg[i][j]); break;
-                case 4: strcpy(seg4, seg[i][j]); break;  // 4 is needed to find 9
+                case 2: strcpy(pat1, seg[i][j]); break;       // 1 is needed later to find 3 and 6
+                case 4: strcpy(pat4, seg[i][j]); break;       // 4 is needed later to find 9
             }
         for (int j = 0; j < 10; ++j)
-            if (len[i][j] == 6 && contains(seg[i][j], seg4))
-                strcpy(seg9, seg[i][j]);
+            if (len[i][j] == 6 && contains(seg[i][j], pat4))  // use 4 to find 9
+                strcpy(pat9, seg[i][j]);                      // 9 is needed later to find 5
 
+        // len 2: "1" = ..c..f.
+        // len 3: "7" = a.c..f.
+        // len 4: "4" = .bcd.f.
+        // len 7: "8" = abcdefg
+        // len 5: "3" = a.cd.fg  step 1: 3 contains 1
+        //        "5" = ab.d.fg  step 2: 9 contains 5
+        //        "2" = a.cde.g  step 3: 2 is left
+        // len 6: "6" = ab.defg  step 1: 6 does not contain 1
+        //        "9" = abcd.fg  step 2: 9 contains 4
+        //        "0" = abc.efg  step 3: 0 is left
+
+        // Determine value of last 4 digits
         int val = 0;
         for (int j = 10; j < M; ++j) {
             val *= 10;
@@ -72,18 +74,18 @@ int main(void)
                 case 3: val += 7; break;
                 case 4: val += 4; break;
                 case 5:
-                    if (contains(seg[i][j], seg1)) {
+                    if (contains(seg[i][j], pat1)) {
                         val += 3;
-                    } else if (contains(seg9, seg[i][j])) {
+                    } else if (contains(pat9, seg[i][j])) {
                         val += 5;
                     } else {
                         val += 2;
                     }
                     break;
                 case 6:
-                    if (!contains(seg[i][j], seg1)) {
+                    if (!contains(seg[i][j], pat1)) {
                         val += 6;
-                    } else if (contains(seg[i][j], seg4)) {
+                    } else if (contains(seg[i][j], pat4)) {
                         val += 9;
                     } else {
                         val += 0;
