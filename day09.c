83a40650980b66368c1e7ced20f3d42eea793725
E. Dronkert <ednl@users.noreply.github.com>
2021-12-09 16:27:47 +0100

    names, maxheight

--- day09.c
+++ day09.c
@@ -14,7 +14,7 @@ typedef struct {
 static unsigned char height[DIM + 2][DIM + 2] = {0};  // extend grid to simplify edge cases
 static bool done[DIM + 2][DIM + 2] = {0};  // keep track of already processed locations
 static Point todo[STACKSIZE] = {0};  // keep track of locations still to be processed
-static int basin[MAXBASINS] = {0};  // size of each basin found
+static int basinsize[MAXBASINS] = {0};  // size of each basin found
 
 // Floodfill the basin, return basin size
 static int fillbasin(int x, int y)
@@ -25,8 +25,8 @@ static int fillbasin(int x, int y)
         Point p = todo[--sp];
         if (!done[p.x][p.y]) {
             done[p.x][p.y] = true;
-            if (height[p.x][p.y] < MAXHEIGHT) {
-                if (sp >= STACKSIZE - 4) {
+            if (height[p.x][p.y] != MAXHEIGHT) {
+                if (sp > STACKSIZE - 4) {
                     fprintf(stderr, "Imminent stack overflow!\n");
                     return -1;
                 }
@@ -41,8 +41,8 @@ static int fillbasin(int x, int y)
     return size;
 }
 
-// Sort int array in reverse order
-static int larger(const void *a, const void *b)
+// Sort int array in descending order
+static int descending(const void *a, const void *b)
 {
     const int p = *(const int*)a;
     const int q = *(const int*)b;
@@ -59,12 +59,12 @@ int main(void)
     }
     fclose(f);
 
-    // Border
+    // None shall pass
     for (int i = 0; i < DIM + 2; ++i)
-        height[i][0] = height[0][i] = height[i][DIM + 1] = height[DIM + 1][i] = MAXHEIGHT + 1;
+        height[i][0] = height[0][i] = height[i][DIM + 1] = height[DIM + 1][i] = MAXHEIGHT;
 
     int risk = 0;
-    size_t count = 0;
+    size_t basincount = 0;
     for (int i = 1; i <= DIM; ++i) {
         for (int j = 1; j <= DIM; ++j) {
             if (height[i][j] < height[i - 1][j] &&
@@ -72,17 +72,17 @@ int main(void)
                 height[i][j] < height[i + 1][j] &&
                 height[i][j] < height[i][j + 1]) {
                 risk += height[i][j] + 1;
-                if (count == MAXBASINS) {
+                if (basincount == MAXBASINS) {
                     fprintf(stderr, "The number of basins is too damn high!\n");
                     return 1;
                 }
-                basin[count++] = fillbasin(i, j);
+                basinsize[basincount++] = fillbasin(i, j);
             }
         }
     }
-    qsort(basin, count, sizeof *basin, larger);
+    qsort(basinsize, basincount, sizeof *basinsize, descending);
 
     printf("Part 1: %d\n", risk);  // 506
-    printf("Part 2: %d\n", basin[0] * basin[1] * basin[2]);  // 931200
+    printf("Part 2: %d\n", basinsize[0] * basinsize[1] * basinsize[2]);  // 931200
     return 0;
 }
