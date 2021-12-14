5f571a103f1397e8e2ca685d4e26fa98b767af31
E. Dronkert <ednl@users.noreply.github.com>
2021-12-14 17:15:35 +0100

    all pairs have rules!

--- day14.c
+++ day14.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-#include <stdint.h>    // int64_t
-#include <limits.h>    // INT64_MAX
-#include <inttypes.h>  // PRId64
-#include <string.h>    // strlen
+#include <stdint.h>    // uint64_t
+#include <limits.h>    // UINT64_MAX
+#include <inttypes.h>  // PRIu64
 #include "startstoptimer.h"
 
-#define BUFLEN   64   // original polymer length = 20 in my input
-#define BUFFRM  "63"  // read BUFLEN-1 chars max
-#define RULES   100   // number of pair replacement rules in my input
-#define ELEMENTS ('Z' - 'A' + 1)  // number of elements = letters in the alphabet
+#define PSIZE  20  // original polymer length = 20 in my input
+#define RULES 100  // number of pair replacement rules in my input
+#define ELMS ('Z' - 'A' + 1)  // number of elements = letters in the alphabet
 
 typedef struct Rule {
     struct Rule *left, *right;  // pointers to replacement pairs (NULL = no replacement)
-    int64_t count, next;        // how many of this pair are there now and next round
+    uint64_t count, next;       // how many of this pair are there now and next round
     char pair[3], ins;          // pair (=2 elements) to replace, 1 element to insert
 } Rule;
 
 static Rule rule[RULES] = {0};
-static int64_t corr[ELEMENTS] = {0};
+static uint64_t subtract[ELMS] = {0};
 
 static inline int ix(const char c)
 {
@@ -32,44 +30,28 @@ static Rule* findrule_c(const char a, const char b)
     return NULL;
 }
 
-static Rule* findrule_s(const char *s)
+static inline Rule* findrule_s(const char *s)
 {
     return findrule_c(s[0], s[1]);
 }
 
-static void read(const char *inp)
+static void parse(const char *inp)
 {
-    char polymer[BUFLEN];
-    int len = 0, n = 0;
-
+    char polymer[PSIZE + 1];  // must be same size as in input file, +1 for \0
     FILE *f = fopen(inp, "r");
-    if (fscanf(f, "%"BUFFRM"s ", polymer) == 1)
-        len = (int)strlen(polymer);
-    while (n < RULES && fscanf(f, "%2s -> %c", rule[n].pair, &rule[n].ins) == 2)
-        ++n;
+    fscanf(f, "%s ", polymer);
+    for (int i = 0; i < RULES && fscanf(f, "%2s -> %c", rule[i].pair, &rule[i].ins) == 2; ++i);
     fclose(f);
 
     // Count elements by pairs in initial polymer
-    if (len > 1) {
-        Rule *r;
-        // First pair
-        if ((r = findrule_s(polymer))) {
-            r->count++;                      // no duplicate element yet
-        } else {
-            corr[ix(polymer[0])]++;          // not found, add both elements individually
-            corr[ix(polymer[1])]++;
-        }
-        // Proceed with next (overlapping) pair
-        for (int i = 1; i < len - 1; ++i)
-            if ((r = findrule_s(&polymer[i]))) {
-                r->count++;                  // add another pair, and
-                corr[ix(polymer[i])]--;      // subtract duplicate element
-            } else
-                corr[ix(polymer[i + 1])]++;  // add only the new element
+    findrule_s(polymer)->count++;          // first pair, no duplicate element yet
+    for (int i = 1; i < PSIZE - 1; ++i) {
+        findrule_s(&polymer[i])->count++;  // add next overlapping pair, and
+        subtract[ix(polymer[i])]++;        // subtract duplicate element
     }
 
-    // Links to replacement pairs
-    for (int i = 0; i < n; ++i) {
+    // Save links to replacement pairs
+    for (int i = 0; i < RULES; ++i) {
         rule[i].left  = findrule_c(rule[i].pair[0], rule[i].ins    );
         rule[i].right = findrule_c(rule[i].ins    , rule[i].pair[1]);
     }
@@ -77,35 +59,25 @@ static void read(const char *inp)
 
 static void grow(void)
 {
-    for (int i = 0; i < RULES; ++i)
-        if (rule[i].count) {
-            if (rule[i].left && rule[i].right) {
-                rule[i].left->next        += rule[i].count;
-                rule[i].right->next       += rule[i].count;
-                corr[ix(rule[i].ins)]     -= rule[i].count;  // subtract duplicate element
-            } else if (rule[i].left) {
-                rule[i].left->next        += rule[i].count;
-                corr[ix(rule[i].pair[1])] += rule[i].count;
-            } else if (rule[i].right) {
-                corr[ix(rule[i].pair[0])] += rule[i].count;
-                rule[i].right->next       += rule[i].count;
-            } else
-                rule[i].next              += rule[i].count;
-        }
+    for (int i = 0; i < RULES; ++i) {
+        rule[i].left->next  += rule[i].count;
+        rule[i].right->next += rule[i].count;
+        subtract[ix(rule[i].ins)] += rule[i].count;  // subtract duplicate element
+    }
     for (int i = 0; i < RULES; ++i) {
         rule[i].count = rule[i].next;
         rule[i].next = 0;  // always reset for next growth cycle!
     }
 }
 
-static int64_t range(const int cycle)
+static uint64_t range(const int cycle)
 {
     // Grow the polymer
     for (int i = 0; i < cycle; ++i)
         grow();
 
     // Count individual elements from pairs
-    int64_t hist[ELEMENTS] = {0};
+    uint64_t hist[ELMS] = {0};
     for (int i = 0; i < RULES; ++i) {
         hist[ix(rule[i].pair[0])] += rule[i].count;
         hist[ix(rule[i].pair[1])] += rule[i].count;
@@ -113,9 +85,9 @@ static int64_t range(const int cycle)
 
     // Correct element count with added and subtracted counts,
     // determine maximum and minimum (non zero), return range
-    int64_t min = INT64_MAX, max = 0;
-    for (int i = 0; i < ELEMENTS; ++i) {
-        hist[i] += corr[i];
+    uint64_t min = UINT64_MAX, max = 0;
+    for (int i = 0; i < ELMS; ++i) {
+        hist[i] -= subtract[i];
         if (hist[i] < min && hist[i])
             min = hist[i];
         if (hist[i] > max)
@@ -127,9 +99,9 @@ static int64_t range(const int cycle)
 int main(void)
 {
     starttimer();
-    read("input14.txt");
-    printf("Part 1: %"PRId64"\n", range(10));  // 2194
-    printf("Part 2: %"PRId64"\n", range(30));  // 2360298895777
+    parse("input14.txt");
+    printf("Part 1: %"PRIu64"\n", range(10));  // 2194
+    printf("Part 2: %"PRIu64"\n", range(30));  // 2360298895777
     printf("%.0f µs\n", stoptimer_us());
     return 0;
 }
