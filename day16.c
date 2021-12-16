ce54d455064472d0a9435fe5c69e1403361a8e72
E. Dronkert <ednl@users.noreply.github.com>
2021-12-16 23:22:54 +0100

    timer

--- day16.c
+++ day16.c
@@ -4,35 +4,18 @@
 #include <inttypes.h>
 #include <limits.h>
 #include <stdbool.h>
+#include "startstoptimer.h"
 
 #define N      1396     // hex chars in input
-#define NIBBLE    4     // hex char: 4 bits
+#define NIBBLE    4     // hex char = 4 bits
 #define M (N * NIBBLE)  // bits in input
-#define VTLEN     3     // version/type: 3 bits
-#define SUBLEN   11
-#define BITLEN   15
+#define VLEN      3     // version = 3 bits
+#define TLEN      3     // type = 3 bits
+#define SLEN     15     // LenID=0: subpacket size  = 15 bits
+#define CLEN     11     // LenID=1: subpacket count = 11 bits
 
 static bool bit[M] = {0};
 static int versionsum = 0;
-
-static int64_t getnumber(unsigned int *index, unsigned int len)
-{
-    int64_t n = 0;
-    while (len--)
-        n = (n << 1) | bit[(*index)++];
-    return n;
-}
-
-static int64_t getliteral(unsigned int *index)
-{
-    int64_t val = 0;
-    bool more;
-    do {
-        more = bit[(*index)++];
-        val = (val << NIBBLE) | getnumber(index, NIBBLE);
-    } while (more);
-    return val;
-}
 
 static inline int64_t min(int64_t a, int64_t b)
 {
@@ -44,22 +27,39 @@ static inline int64_t max(int64_t a, int64_t b)
     return a > b ? a : b;
 }
 
-// Read packet and subpackets, updates global versionsum, returns number of bit consumed
-static int64_t getpacket(unsigned int *index)
+static int64_t getnumber(int *index, int len)
 {
-    versionsum += (int)getnumber(index, VTLEN);
-    int8_t typeid = (int8_t)getnumber(index, VTLEN);
+    int64_t n = 0;
+    while (len--)
+        n = (n << 1) | bit[(*index)++];
+    return n;
+}
+
+static int64_t getliteral(int *index)
+{
+    int64_t val = 0;
+    bool more;
+    do {
+        more = bit[(*index)++];
+        val = (val << NIBBLE) | getnumber(index, NIBBLE);
+    } while (more);
+    return val;
+}
+
+static int64_t getpacket(int *index)
+{
+    versionsum += (int)getnumber(index, VLEN);
+    int typeid = (int)getnumber(index, TLEN);
     if (typeid == 4)
         return getliteral(index);
 
-    bool lengthid = bit[(*index)++];
-    int maxsubs, maxbits;
-    if (lengthid) {
-        maxsubs = (int)getnumber(index, SUBLEN);
-        maxbits = INT_MAX;
+    int count, size;
+    if (bit[(*index)++]) {
+        count = (int)getnumber(index, CLEN);
+        size  = INT_MAX;
     } else {
-        maxsubs = INT_MAX;
-        maxbits = (int)getnumber(index, BITLEN);
+        count = INT_MAX;
+        size  = (int)getnumber(index, SLEN);
     }
     int64_t val = 0;
     if (typeid < 4) {
@@ -68,8 +68,8 @@ static int64_t getpacket(unsigned int *index)
             case 2: val = INT64_MAX; break;  // min
             case 3: val = INT64_MIN; break;  // max
         }
-        while (maxsubs > 0 && maxbits > 0) {
-            int i = (int)*index;
+        while (count > 0 && size > 0) {
+            int ix = *index;
             int64_t a = getpacket(index);
             switch (typeid) {
                 case 0: val += a; break;  // sum
@@ -77,15 +77,15 @@ static int64_t getpacket(unsigned int *index)
                 case 2: val = min(val, a); break;  // min
                 case 3: val = max(val, a); break;  // max
             }
-            maxsubs--;
-            maxbits -= (int)*index - i;
+            count--;
+            size -= *index - ix;
         }
     } else {
         int64_t a = getpacket(index);
         int64_t b = getpacket(index);
         switch (typeid) {
-            case 5: val = a > b; break;  // gt
-            case 6: val = a < b; break;  // lt
+            case 5: val = a > b;  break;  // gt
+            case 6: val = a < b;  break;  // lt
             case 7: val = a == b; break;  // eq
         }
     }
@@ -94,6 +94,8 @@ static int64_t getpacket(unsigned int *index)
 
 int main(void)
 {
+    starttimer();
+
     FILE *f = fopen("input16.txt", "r");
     char hex[17] = {0};
     int k = 0;
@@ -101,17 +103,19 @@ int main(void)
         char *end;
         uint64_t val = strtoull(hex, &end, 16);
         ssize_t bitlen = (end - hex) * 4;
-        uint64_t mask = (1ULL << (bitlen - 1));
+        uint64_t mask = UINT64_C(1) << (bitlen - 1);
         while (mask) {
-            bit[k++] = (val & mask) != 0;
+            bit[k++] = val & mask;
             mask >>= 1;
         }
     }
     fclose(f);
 
-    unsigned int i = 0;
+    int i = 0;
     int64_t part2 = getpacket(&i);
     printf("Part 1: %d\n", versionsum);  // 936
-    printf("Part 2: %lld\n", part2);     // ?
+    printf("Part 2: %lld\n", part2);     // 6802496672062
+
+    printf("%.0f µs\n", stoptimer_us());
     return 0;
 }
