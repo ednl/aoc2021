52697c147d048a863458f2d213c9e4ac4dc0f9e9
E. Dronkert <ednl@users.noreply.github.com>
2021-12-23 12:19:25 +0100

    day 22 refactor & faster

--- day22.c
+++ day22.c
@@ -2,127 +2,114 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define N 101  // size of dimensions part 1
-#define M  20  // cube count part 1
-static bool cube[N << 14] = {0};
-
-#define N2 7000  // max 6861 for my input
-#define M2 1300  // max 1252 for my input
+static const char *fname = "input22.txt";
+#define L   20  // number of lines in input file for part 1
+#define N 4500  // max 4199 for my input
+#define M  350  // max  337 for my input
 typedef struct {
     int x0, x1, y0, y1, z0, z1, count;
 } Cube;
-static Cube cub[N2] = {0};
-static Cube upd[N2] = {0};
+static Cube cube[N] = {0};
+static Cube temp[M] = {0};
+static int len = 0;  // length of cube[]
+
+// Intersect cube a with b, save result in c if non-empty, return whether non-empty
+static bool intersect(const Cube *const a, const Cube *const b, Cube *const c)
+{
+    if (a->x0 <= b->x1 && a->x1 >= b->x0 &&
+        a->y0 <= b->y1 && a->y1 >= b->y0 &&
+        a->z0 <= b->z1 && a->z1 >= b->z0) {
+        *c = (Cube){
+            a->x0 >= b->x0 ? a->x0 : b->x0, a->x1 <= b->x1 ? a->x1 : b->x1,
+            a->y0 >= b->y0 ? a->y0 : b->y0, a->y1 <= b->y1 ? a->y1 : b->y1,
+            a->z0 >= b->z0 ? a->z0 : b->z0, a->z1 <= b->z1 ? a->z1 : b->z1,
+            -a->count};  // opposite count to first cube
+        return true;
+    }
+    return false;
+}
+
+// Find cube b in cube counter array a, return pointer to cube in a, or NULL
+static Cube* find(Cube *const a, const Cube *const b, const int alen)
+{
+    int i = 0;
+    while (i < alen && (
+        a[i].x0 != b->x0 || a[i].x1 != b->x1 ||
+        a[i].y0 != b->y0 || a[i].y1 != b->y1 ||
+        a[i].z0 != b->z0 || a[i].z1 != b->z1)) {
+        i++;
+    }
+    if (i < alen)
+        return &a[i];
+    return NULL;
+}
+
+// Add one cube b to cube counter array a, return new length of array
+static int add(Cube *const a, const Cube *const b, const int alen, const int maxlen)
+{
+    if (!b->count)               // only add cube with non-zero count
+        return alen;
+    Cube *c = find(a, b, alen);  // find cube b in cube counter array a
+    if (c) {
+        c->count += b->count;    // add count to existing cube counter already in array
+        return alen;
+    }
+    if (alen < maxlen) {
+        a[alen] = *b;            // append new cube to cube counter array
+        return alen + 1;         // length increased by 1
+    }
+    fprintf(stderr, "Cube counter array overflow while adding (max=%d)\n", maxlen);
+    return -1;
+}
+
+// Merge two cube counter arrays, return new length of first array
+static int merge(Cube *const a, const Cube *const b, const int alen, const int blen, const int maxlen)
+{
+    int n = alen;                            // start appending from here
+    for (int i = 0; i < blen; ++i)
+        // n = add(a, &b[i], n, maxlen);     // one-liner if b needs to be checked for duplicate cubes
+        // But faster if b contains unique cubes already:
+        if (b[i].count) {                    // only merge cubes with non-zero count
+            Cube *c = find(a, &b[i], alen);  // only check to old length = assumes b is already unique
+            if (c)
+                c->count += b[i].count;      // add count to existing cube counter already in array
+            else if (n < maxlen)
+                a[n++] = b[i];               // append new cube from b to cube counter array a
+            else {
+                fprintf(stderr, "Cube counter array overflow while merging (max=%d)\n", maxlen);
+                return -1;
+            }
+        }
+    return n;
+}
+
+static int64_t read(FILE *f, const int lines)
+{
+    char buf[4];
+    int x0, x1, y0, y1, z0, z1;
+    for (int i = 0; (!lines || i < lines) && fscanf(f, "%3s x=%d..%d,y=%d..%d,z=%d..%d ", buf, &x0, &x1, &y0, &y1, &z0, &z1) == 7; ++i) {
+        Cube b = (Cube){x0, x1, y0, y1, z0, z1, buf[2] == '\0' ? 1 : -1}, c;
+        int m = 0;
+        for (int j = 0; j < len; ++j)
+            if (intersect(&cube[j], &b, &c))
+                m = add(temp, &c, m, M);
+        if (b.count == 1)
+            m = add(temp, &b, m, M);
+        len = merge(cube, temp, len, m, N);
+    }
+    int64_t on = 0;
+    for (int i = 0; i < len; ++i) {
+        Cube *a = &cube[i];
+        on += (int64_t)a->count * (a->x1 - a->x0 + 1) * (a->y1 - a->y0 + 1) * (a->z1 - a->z0 + 1);
+    }
+    return on;
+}
 
 int main(void)
 {
-    FILE *f = fopen("input22.txt", "r");
-    char buf[4];
-    int x0, x1, y0, y1, z0, z1;
-    for (int n = 0; n < M && fscanf(f, "%3s x=%d..%d,y=%d..%d,z=%d..%d ", buf, &x0, &x1, &y0, &y1, &z0, &z1) == 7; ++n) {
-        x0 += 50; x1 += 51;
-        y0 += 50; y1 += 51;
-        z0 += 50; z1 += 51;
-        for (int x = x0; x < x1; ++x) {
-            int i = x << 14;
-            for (int y = y0; y < y1; ++y) {
-                int j = i | (y << 7);
-                for (int z = z0; z < z1; ++z)
-                    cube[j | z] = !buf[2];
-            }
-        }
-    }
+    FILE *f = fopen(fname, "r");
+    printf("Part 1: %lld\n", read(f, L));  // 547648
+    printf("Part 2: %lld\n", read(f, 0));  // 1206644425246111
     fclose(f);
-    int64_t on = 0;
-    for (int i = 0; i < N << 14; ++i)
-        on += cube[i];
-    printf("Part 1: %lld\n", on);  // 547648
-
-    f = fopen("input22.txt", "r");
-    int n = 0, m;
-    while (fscanf(f, "%3s x=%d..%d,y=%d..%d,z=%d..%d ", buf, &x0, &x1, &y0, &y1, &z0, &z1) == 7) {
-        m = 0;
-        for (int i = 0; i < n; ++i) {
-            if (cub[i].count) {
-                Cube *c = &cub[i];  // convenience reference to existing cube
-                Cube d = (Cube){  // intersection of existing and new cube
-                    x0 >= c->x0 ? x0 : c->x0, x1 <= c->x1 ? x1 : c->x1,
-                    y0 >= c->y0 ? y0 : c->y0, y1 <= c->y1 ? y1 : c->y1,
-                    z0 >= c->z0 ? z0 : c->z0, z1 <= c->z1 ? z1 : c->z1,
-                    -c->count};  // opposite count to existing
-                if (d.x0 <= d.x1 && d.y0 <= d.y1 && d.z0 <= d.z1) {  // intersection not empty?
-                    int j = 0;
-                    while (j < m && (  // find same cube in current update array
-                        d.x0 != upd[j].x0 || d.x1 != upd[j].x1 ||
-                        d.y0 != upd[j].y0 || d.y1 != upd[j].y1 ||
-                        d.z0 != upd[j].z0 || d.z1 != upd[j].z1)) {
-                        ++j;
-                    }
-                    if (j < m) {
-                        // add to existing
-                        upd[j].count += d.count;
-                    } else {
-                        // add new
-                        if (j == M2) {
-                            fprintf(stderr, "Cube upd overflow\n");
-                            return 1;
-                        }
-                        upd[m++] = d;
-                    }
-                }
-            }
-        }
-        if (buf[2] == '\0') {  // "on" => add new cube to upd array
-            int j = 0;
-            while (j < m && (  // find same cube in current update array
-                x0 != upd[j].x0 || x1 != upd[j].x1 ||
-                y0 != upd[j].y0 || y1 != upd[j].y1 ||
-                z0 != upd[j].z0 || z1 != upd[j].z1)) {
-                ++j;
-            }
-            if (j < m) {
-                // add to existing
-                upd[j].count++;
-            } else {
-                // add new
-                if (j == M2) {
-                    fprintf(stderr, "Cube upd overflow\n");
-                    return 2;
-                }
-                upd[m++] = (Cube){x0, x1, y0, y1, z0, z1, 1};
-            }
-        }
-        // Add upd array to main cube array
-        int nn = n;
-        for (int i = 0; i < m; ++i) {  // for every cube in the upd array
-            Cube *c = &upd[i];
-            int j = 0;
-            while (j < n && (  // find same cube in main cube array
-                c->x0 != cub[j].x0 || c->x1 != cub[j].x1 ||
-                c->y0 != cub[j].y0 || c->y1 != cub[j].y1 ||
-                c->z0 != cub[j].z0 || c->z1 != cub[j].z1)) {
-                ++j;
-            }
-            if (j < n) {
-                // add to existing
-                cub[j].count += c->count;
-            } else {
-                // add new
-                if (nn == N2) {
-                    fprintf(stderr, "Cube main overflow\n");
-                    return 2;
-                }
-                cub[nn++] = *c;
-            }
-        }
-        n = nn;
-    }
-    fclose(f);
-    on = 0;
-    for (int i = 0; i < n; ++i) {
-        Cube *c = &cub[i];
-        on += (int64_t)c->count * (c->x1 - c->x0 + 1) * (c->y1 - c->y0 + 1) * (c->z1 - c->z0 + 1);
-    }
-    printf("Part 2: %lld\n", on);  // 1206644425246111
     return 0;
 }
