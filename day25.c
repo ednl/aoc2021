42945313515415dba7f2da7ccc38ec2e6eaa7510
E. Dronkert <ednl@users.noreply.github.com>
2021-12-25 13:21:45 +0100

    day 25

--- day25.c
+++ day25.c
@@ -1,14 +1,53 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+static const char *fname = "input25.txt";
 #define W 139
 #define H 137
 static char a[2][H][W] = {0};
 
+static bool evolve(void)
+{
+    char c;
+    bool moved = false;
+    // East
+    for (int i = 0; i < H; ++i)
+        for (int j = 0; j < W; )
+            if ((c = a[0][i][j]) == '>') {
+                int k = j + 1;
+                if (k == W)
+                    k = 0;
+                if (a[0][i][k] == '.') {
+                    a[1][i][j] = '.';
+                    a[1][i][k] = c;
+                    j += 2;
+                    moved = true;
+                } else
+                    a[1][i][j++] = c;
+            } else
+                a[1][i][j++] = c;
+    // South
+    for (int j = 0; j < W; ++j)
+        for (int i = 0; i < H; )
+            if ((c = a[1][i][j]) == 'v') {
+                int k = i + 1;
+                if (k == H)
+                    k = 0;
+                if (a[1][k][j] == '.') {
+                    a[0][i][j] = '.';
+                    a[0][k][j] = c;
+                    i += 2;
+                    moved = true;
+                } else
+                    a[0][i++][j] = c;
+            } else
+                a[0][i++][j] = c;
+    return moved;
+}
+
 int main(void)
 {
-    // Read input file
-    FILE *f = fopen("input25.txt", "r");
+    FILE *f = fopen(fname, "r");
     for (int i = 0; i < H; ++i) {
         for (int j = 0; j < W; ++j)
             a[0][i][j] = (char)fgetc(f);
@@ -16,24 +55,9 @@ int main(void)
     }
     fclose(f);
 
-    bool moved = false;
-    // Move east
-    for (int i = 0; i < H; ++i)
-        for (int j = 0; j < W; ++j)
-            if (a[0][i][j] == '>') {
-                int k = (j + 1) % W;
-                if (a[0][i][k] == '.') {
-                    a[1][i][j] = '.';
-                    a[1][i][k] = '>';
-                    ++j;
-                    moved = true;
-                } else {
-                    a[1][i][j] = '>';
-                }
-            } else
-                a[1][i][j] = a[0][i][j];
-    for (int i = 0; i < H; ++i)
-        a[0][i][0] = a[0][i][W];
-
+    int step = 1;  // strange halting condition in the puzzle
+    while (evolve())
+        ++step;
+    printf("Part 1: %d\n", step);
     return 0;
 }
